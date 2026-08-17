1241e64cc92af5f5824fab10133ebf649b0a5cd0
averyocean65 <averytoad65@gmail.com>
2026-08-17 11:21:54 +0200

    init project with template

---
+++ ARL/ARL.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ARL;
+
+[BepInPlugin("averyocean65.ARL", "ARL", "1.0")]
+public class ARL : BaseUnityPlugin
+{
+    internal static ARL Instance { get; private set; } = null!;
+    internal new static ManualLogSource Logger => Instance._logger;
+    private ManualLogSource _logger => base.Logger;
+    internal Harmony? Harmony { get; set; }
+
+    private void Awake()
+    {
+        Instance = this;
+        
+        // Prevent the plugin from being deleted
+        gameObject.transform.parent = null;
+        gameObject.hideFlags = HideFlags.HideAndDontSave;
+
+        Patch();
+
+        Logger.LogInfo($"{Info.Metadata.GUID} v{Info.Metadata.Version} has loaded!");
+    }
+
+    internal void Patch()
+    {
+        Harmony ??= new Harmony(Info.Metadata.GUID);
+        Harmony.PatchAll();
+    }
+
+    internal void Unpatch()
+    {
+        Harmony?.UnpatchSelf();
+    }
+}
