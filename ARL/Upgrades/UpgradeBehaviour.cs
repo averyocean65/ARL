cfc82f41f141ca63130132835ce21023fd71474c
averyocean65 <averytoad65@gmail.com>
2026-08-21 18:00:51 +0200

    work on UpgradeBehaviour

---
+++ ARL/Upgrades/UpgradeBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using Steamworks;
+using UnityEngine;
+
+namespace ARL.Upgrades;
+
+public class UpgradeBehaviour : MonoBehaviour {
+	protected ItemToggle _toggle;
+	private FieldInfo _photonIdField;
+
+	protected virtual void Awake() {
+		_toggle = GetComponent<ItemToggle>();
+		GetDataFields();
+	}
+
+	private void GetDataFields() {
+		_photonIdField = AccessTools.Field(typeof(ItemToggle), "playerTogglePhotonID");
+	}
+
+	protected virtual void Upgrade() {
+		string steamId = "";
+
+		try {
+			int photonId = (int)_photonIdField.GetValue(_toggle);
+			PlayerAvatar playerAvatar = SemiFunc.PlayerAvatarGetFromPhotonID(photonId);
+			steamId = SemiFunc.PlayerGetSteamID(playerAvatar);
+		}
+		catch {
+			steamId = SteamClient.SteamId.ToString();
+		}
+		
+		// TODO: call function to perform the upgrade in REPO's logic
+	}
+}
