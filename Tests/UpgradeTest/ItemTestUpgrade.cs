8ab0824d8fc85e28a6269ec6bbd407034fdcdd53
averyocean65 <averytoad65@gmail.com>
2026-08-22 13:05:46 +0200

    change ItemTestUpgrade to conform to new standard

--- Tests/UpgradeTest/ItemTestUpgrade.cs
+++ Tests/UpgradeTest/ItemTestUpgrade.cs
@@ -4,10 +4,6 @@ using ARL.Upgrades;
 namespace UpgradeTest;
 
 public class ItemTestUpgrade : UpgradeBehaviour {
-	protected override CustomUpgrade GetUpgradeInfo() {
-		return UpgradeManager.Instance.RegisterOrFetchUpgrade("averyocean65.upgradetest.testone", "Test One");
-	}
-	
 	protected override void PerformUpgrade(bool isInLevel) {
 		UpgradeTest.Logger.LogInfo($"Applied upgrade! Is in level: {isInLevel}; Amount: {GetUpgradeInfo()
 			.UpgradeDictionary
